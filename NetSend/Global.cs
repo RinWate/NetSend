933fdd9ce222df10d5633754f87f61a0908831be
Denis Levenger <42698624+RinWate@users.noreply.github.com>
2025-04-25 00:48:35 +0300

    24042025.5 - Исправлена ошибка в заголовке диалога массовой отправки - Исправлена ошибка, при которой не сохранялись комментарий в списке игнорируемых получателей

--- NetSend/Global.cs
+++ NetSend/Global.cs
@@ -8,7 +8,7 @@ using System.Collections.ObjectModel;
 namespace NetSend {
 	public static class Global {
 
-		public static readonly string[] TITLES = { @"Режим ""Глашатый""", "Наведём суету?", "Орём во весь голос?", "Устроим спам?", "У меня окошко вылезло..." };
+		public static readonly string[] TITLES = { @"Режим ""Глашатай""", "Наведём суету?", "Орём во весь голос?", "Устроим спам?", "У меня окошко вылезло..." };
 		public static readonly string VERSION = "v 1.3.1 Native";
 		public static ObservableCollection<Recipient> Recipients { get; set; } = new ObservableCollection<Recipient>();
 		public static ObservableCollection<IgnoredRecipient> IgnoredRecipients { get; set; } = new ObservableCollection<IgnoredRecipient>();
